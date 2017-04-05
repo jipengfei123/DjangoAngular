6dc5a23d163dc6dfad9de7761c8be28c56046271
jipf <jipengfei313@163.com>
2017-04-05 15:51:43 +0800

    参数请求

--- DjangoAngular/urls.py
+++ DjangoAngular/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.views.static import serve
 from . import view
 from . import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^helloWorld/', view.helloWorld),
@@ -25,6 +26,15 @@ urlpatterns = [
     url(r'^helloJson2/', view.helloJson2),
     url(r'^helloModel/', view.helloModel),
     url(r'^helloTemplates/', view.helloTemplates),
+    url(r'^helloParam1/(.+)/$', view.helloParam1),
+    # http://127.0.0.1:8000/helloParam1/pathparam/
+    url(r'^helloParam2/(.+)/(.+)/$', view.helloParam2),
+    # http://127.0.0.1:8000/helloParam2/pathparam1/pathparam2/
+    url(r'^helloParam3/p1(\w+)p2(.+)/$', view.helloParam3),
+    # http://127.0.0.1:8000/helloParam3/p1pathparam1p2pathparam2/
+    url(r'^helloParam4/', view.helloParam4),
+    # http://127.0.0.1:8000/helloParam4/?p1=param1&p2=param2
+    url(r'^helloParam5$', view.helloParam5),
     # ...... 其他配置
     url(r'^angular/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
--- DjangoAngular/view.py
+++ DjangoAngular/view.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
 from . import models
-import json
 
 
 def helloTemplates(request):
@@ -12,29 +12,19 @@ def helloTemplates(request):
 
 
 def helloModel(request):
+    # testmodel = models.Hotel(name="单人房", cf=20, br=15)
+    # testmodel.save()
     testModels = models.Hotel.objects.all()
     json_data = serializers.serialize("json", testModels)
     return HttpResponse(json_data, content_type="application/json")
 
 
-def helloWorld(request):
-    return HttpResponse("Hello world ! ")
-
-
-def helloJson1(request):
-    json_data = [{'name': 'zhangsan', 'age': 30}]
-    return HttpResponse(json_data, content_type="application/json")
-
-
-def helloJson2(request):
-    modelhello = models.Student("zhangsan", 20)
-    return HttpResponse([modelhello.__dict__], content_type="application/json")
-    '''
+'''
 
 
 
 
-    Person.objects.all() # 查询所有
+ Person.objects.all() # 查询所有
 Person.objects.all()[:10] 切片操作，获取10个人，不支持负索引，切片可以节约内存，不支持负索引，后面有相应解决办法，第7条
 Person.objects.get(name="WeizhongTu") # 名称为 WeizhongTu 的一条，多条会报错
 
@@ -51,4 +41,50 @@ Person.objects.filter(name__iregex="^abc")# 正则表达式不区分大小写
 # filter是找出满足条件的，当然也有排除符合某条件的
 Person.objects.exclude(name__contains="WZ") # 排除包含 WZ 的Person对象
 Person.objects.filter(name__contains="abc").exclude(age=23) # 找出名称含有abc, 但是排除年龄是23岁的
-    '''
+ '''
+
+
+def helloWorld(request):
+    return HttpResponse("Hello world ! ")
+
+
+def helloJson1(request):
+    json_data = [{'name': 'zhangsan', 'age': 30}]
+    return HttpResponse(json_data, content_type="application/json")
+
+
+def helloJson2(request):
+    modelhello = models.Student("zhangsan", 20)
+    return HttpResponse([modelhello.__dict__], content_type="application/json")
+
+
+def helloParam1(request, param):
+    return HttpResponse("param: " + param)
+
+
+def helloParam2(request, param1, param2):
+    return HttpResponse("param1 = " + param1 + "   <br>param2 = " + param2)
+
+
+def helloParam3(request, param1, param2):
+    return HttpResponse("param1 = " + param1 + "   <br>param2 = " + param2)
+
+
+def helloParam4(request):
+    p1 = request.GET.get('p1')
+    p2 = request.GET.get('p2')
+    return HttpResponse("p1 = " + p1 + "   <br>p2 = " + p2)
+
+
+@csrf_exempt
+def helloParam5(request):
+    # p1 = request.POST.get('p1')
+    # p2 = request.POST.get('p2')
+    if request.POST:
+        paramObj = request.POST
+        print(paramObj)
+        lists = paramObj.getlist(' name')
+        a= lists[0]
+        print(lists[0])
+        print(lists[1])
+    return HttpResponse("ok")
